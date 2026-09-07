c1045e65f7026435cea0257f1f3878093a5eeba1
sharawla1995-oss <sharawla1995@gmail.com>
2026-09-08 00:20:54 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='restaurant-pos-v9.8.0';
-const SHELL=['./','./index.html','./styles.css?v=9.8.0','./app.js?v=9.8.0','./manifest.json'];
+const CACHE='restaurant-pos-v9.8.1';
+const SHELL=['./','./index.html','./styles.css?v=9.8.1','./app.js?v=9.8.1','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(x=>x.put(e.request,cp));return r}).catch(()=>caches.match('./index.html'))));});
