103cd3fd41d1271f20376c83fefa39c9f97b839a
sharawla1995-oss <sharawla1995@gmail.com>
2026-09-07 12:56:27 +0300

    Update cache version to 'top-burger-pos-v9-2-2'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='top-burger-pos-v9-2-1';
+const CACHE='top-burger-pos-v9-2-2';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
