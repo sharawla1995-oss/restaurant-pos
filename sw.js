3c20fb76f2d6a59100b3a6896a46b0e8c9919684
sharawla1995-oss <sharawla1995@gmail.com>
2026-09-07 03:33:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='restaurant-pos-v9-0-stable';
+const CACHE='restaurant-pos-v9-1-1-menu';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
