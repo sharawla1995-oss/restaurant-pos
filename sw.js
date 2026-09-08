3bf9b7fb33a3f9d3c1075809da01abdc3822f220
sharawla1995-oss <sharawla1995@gmail.com>
2026-09-09 01:42:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='sharawla-pos-v10.4.15';
-const SHELL=['./','./index.html','./styles.css?v=10.4.15','./app.js?v=10.4.15','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='sharawla-pos-v10.4.16';
+const SHELL=['./','./index.html','./styles.css?v=10.4.16','./app.js?v=10.4.16','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
