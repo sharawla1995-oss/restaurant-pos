78de678bec93f070ba5a27e1fabc49b88859cb33
sharawla1995-oss <sharawla1995@gmail.com>
2026-09-08 15:22:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='top-burger-pos-v10.4.7';
-const SHELL=['./','./index.html','./styles.css?v=10.4.7','./app.js?v=10.4.7','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='top-burger-pos-v10.4.8';
+const SHELL=['./','./index.html','./styles.css?v=10.4.8','./app.js?v=10.4.8','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
